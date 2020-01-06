8f08c5dd244580346d84212ba9571c21e38596c3
Dmitry Sedykh <sedykh@gmail.com>
2020-01-06 22:45:51 +0300

    abstract metadata format

--- abstract.go
+++ abstract.go
@@ -7,11 +7,17 @@ import (
 )
 
 // Abstract is a default not specified metadata format.
-type Abstract []yaml.Node
+type Abstract yaml.Node
 
 // Encode encode metadata to YAML.
 func (a Abstract) Encode(w io.Writer) error {
+	if len(a.Content) == 0 {
+		return nil
+	}
 	enc := yaml.NewEncoder(w)
-	defer enc.Close()
-	return enc.Encode(a)
+	if err := enc.Encode(a.Content[0]); err != nil {
+		enc.Close()
+		return err
+	}
+	return enc.Close()
 }
