c6f29306d395c40e0c4f1798970cda1cee8a0662
Dmitry Sedykh <sedykh@gmail.com>
2020-01-06 23:01:35 +0300

    IsZero

--- abstract.go
+++ abstract.go
@@ -9,9 +9,14 @@ import (
 // Abstract is a default not specified metadata format.
 type Abstract yaml.Node
 
+// IsZero return true if metadata not defined.
+func (a Abstract) IsZero() bool {
+	return len(a.Content) == 0
+}
+
 // Encode encode metadata to YAML.
 func (a Abstract) Encode(w io.Writer) error {
-	if len(a.Content) == 0 {
+	if a.IsZero() {
 		return nil
 	}
 	enc := yaml.NewEncoder(w)
